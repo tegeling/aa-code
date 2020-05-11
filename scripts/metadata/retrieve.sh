b8273ee88502a4fd5c0ceff5eda980f40cb9b8d9
Thomas Egeling <tegeling@tegeling-ltm1.internal.salesforce.com>
2020-05-11 10:51:40 +0200

    Updated API version and format of Apex

--- scripts/metadata/retrieve.sh
+++ scripts/metadata/retrieve.sh
@@ -1,12 +1,14 @@
+## clean up files in mdapi
+cd mdapi && rm aa-code.zip && rm -r aa-code && cd ..
+
 ## Retrieve the changes from the developer sandbox.
-sfdx force:source:retrieve --manifest mdapi/package.xml
+## sfdx force:source:retrieve --manifest mdapi/package.xml
 
 ## Convert the source to metadata format
-sfdx force:source:convert --rootdir force-app --outputdir mdapi
+sfdx force:source:convert --rootdir force-app --outputdir mdapi/aa-code
 
 ## Create a .zip file of the contents in the tmp_convert directory.
-cd mdapi
-rm aa-code.zip && zip -r aa-code.zip *
+cd mdapi && zip -r aa-code.zip *
 
 ## Run the deploy command
 ## sfdx force:mdapi:deploy --zipfile mdapi/aa-code.zip --testlevel RunSpecifiedTests --runtests TestMyCode
@@ -15,14 +17,10 @@ rm aa-code.zip && zip -r aa-code.zip *
 sfdx force:mdapi:deploy --checkonly --zipfile aa-code.zip --testlevel RunLocalTests --wait=-1
 
 ## check status
-sfdx force:mdapi:deploy:report --jobid=0Af5E00001BMXiqSAH
-
-
-## Test the actual production deployment
-sfdx force:mdapi:deploy --checkonly --zipfile mdapi_output/winter19.zip --targetusername full-sandbox --testlevel RunSpecifiedTests TestLanguageCourseTrigger
+sfdx force:mdapi:deploy:report --jobid=0Af5E00001BNMTrSAP
 
 ## test the quick deploy using the job ID returned in the previous step
-sfdx force:mdapi:deploy --targetusername full-sandbox --validateddeployrequestid jobID
+sfdx force:mdapi:deploy --targetusername aa-release --validateddeployrequestid=0Af5E00001BNMTrSAP --wait=-1
 
 ## convert your files from source format to metadata format
 sfdx force:source:convert
