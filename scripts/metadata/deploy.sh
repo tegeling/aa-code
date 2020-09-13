57e4010fe04e3f4a6064deefec730d9fd66d7a19
Thomas Egeling <tegeling@tegeling-ltm1.internal.salesforce.com>
2020-09-13 14:34:38 +0200

    Summer20v3 released

--- scripts/metadata/deploy.sh
+++ scripts/metadata/deploy.sh
@@ -1,12 +1,12 @@
 ## $Id$
 ## validate the deployment without saving 
-sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Summer20v2.zip --testlevel RunLocalTests --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Summer20v3.zip --testlevel RunLocalTests --wait=-1
 
 ## check status
 ## sfdx force:mdapi:deploy:report --jobid=0Af1p00001Q45KkCAJ
 
 ## Deploy and skip validation using quick deploy with job ID returned in the previous step
-sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af1p00001RXaMPCA1 --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af1p00001RXvV8CAL --wait=-1
 
 ## Cancel deployment
 ##sfdx force:mdapi:deploy:cancel -i 0Af1p00001Q45KkCAJ
