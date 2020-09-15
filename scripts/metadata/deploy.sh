25612a39f5df6de3cd76095024fad2586f11fba2
Thomas Egeling <tegeling@tegeling-ltm1.internal.salesforce.com>
2020-09-15 15:58:54 +0200

    Changed MeinUrlaub Post signature

--- scripts/metadata/deploy.sh
+++ scripts/metadata/deploy.sh
@@ -1,12 +1,11 @@
-## $Id$
 ## validate the deployment without saving 
-sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Summer20v3.zip --testlevel RunLocalTests --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Summer20v4.zip --testlevel RunLocalTests --wait=-1
 
 ## check status
 ## sfdx force:mdapi:deploy:report --jobid=0Af1p00001Q45KkCAJ
 
 ## Deploy and skip validation using quick deploy with job ID returned in the previous step
-sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af1p00001RXvV8CAL --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af1p00001RY5RaCAL --wait=-1
 
 ## Cancel deployment
 ##sfdx force:mdapi:deploy:cancel -i 0Af1p00001Q45KkCAJ
