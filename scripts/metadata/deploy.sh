f1700f5bb8dc47681bb3b0b217ca11c3c153cdc2
Thomas Egeling <thomas.egeling@googlemail.com>
2021-01-31 14:22:37 +0100

    Winter21v10 released

--- scripts/metadata/deploy.sh
+++ scripts/metadata/deploy.sh
@@ -1,11 +1,11 @@
 ## validate the deployment without saving 
-sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Winter21v9.zip --testlevel RunLocalTests --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Winter21v10.zip --testlevel RunLocalTests --wait=-1
 
 ## check status
 ## sfdx force:mdapi:deploy:report --jobid=0Af1p00001Q45KkCAJ
 
 ## Deploy and skip validation using quick deploy with job ID returned in the previous step
-sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af0800001SZHNgCAP --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af0800001SaraJCAR --wait=-1
 
 ## Cancel deployment
 ##sfdx force:mdapi:deploy:cancel -i 0Af1p00001Q45KkCAJ
