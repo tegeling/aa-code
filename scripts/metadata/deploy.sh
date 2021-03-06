9b5fc4873de716854ad916bdd35d87485fe0a2da
Thomas Egeling <thomas.egeling@googlemail.com>
2021-03-06 14:37:45 +0100

    Changed gender filter and deployed new release

--- scripts/metadata/deploy.sh
+++ scripts/metadata/deploy.sh
@@ -1,11 +1,11 @@
 ## validate the deployment without saving 
-sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Winter21v14.zip --testlevel RunLocalTests --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --checkonly --zipfile releases/Winter21v15.zip --testlevel RunLocalTests --wait=-1
 
 ## check status
-## sfdx force:mdapi:deploy:report --jobid=0Af1p00001Q45KkCAJ
+## sfdx force:mdapi:deploy:report --jobid=0Af0E00001oJiG0SAK
 
 ## Deploy and skip validation using quick deploy with job ID returned in the previous step
-sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af0800001SxNGACA3 --wait=-1
+sfdx force:mdapi:deploy --targetusername aa-production --validateddeployrequestid=0Af0800001SnuEgCAJ --wait=-1
 
 ## Cancel deployment
 ##sfdx force:mdapi:deploy:cancel -i 0Af1p00001Q45KkCAJ
